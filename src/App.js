5abd9c179a71b5df92f24210ea720fefe67bd5d8
El-Nico <nicholasc1665@yahoo.com>
2023-05-11 15:43:27 +0100

    improved frontend layout

--- src/App.js
+++ src/App.js
@@ -14,95 +14,36 @@ import {
 import { useNavigate } from "react-router-dom";
 
 function App() {
-  ////////////////////////zip stuff//////////
-  // testa();
-  //downloadFile(tutorials.css, "xy.zip");
-  // getgit("El-Nico", "css_tutorials", "README.md").then((data) => {
-  //   console.log("data shortly");
-  //   console.log(data);
-  // });
-  // const navigate = useNavigate();
-
-  // window.location.assign(
-  //   "https://github.com/El-Nico/css_tutorials/archive/refs/heads/main.zip"
-  // );
-
-  getgit2("El-Nico", "fewd_lab_3", "01_lesson").then((data) => {
-    console.log(data);
-  });
-  //////////////////end of zip stuff//////////////
-  const [imageUpload, setImageUpload] = useState(null);
-  const [imageList, setImageList] = useState([]);
-
-  const imageListRef = ref(storage, "images/");
-  const uploadImage = () => {
-    if (imageUpload == null) return;
-    // const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
-    // uploadBytes(imageRef, imageUpload).then((snapshot) => {
-    //   alert("Image uploaded");
-    //   getDownloadURL(snapshot.ref).then((url) => {
-    //     setImageList((prev) => [...prev, url]);
-    //   });
-    // });
-    /////////////////////ma own jargon//////////////////////////////
-
-    const myFile = URLtoFile(
-      "https://i.insider.com/602d763842b84000192f4072?width=1136&format=jpeg"
-    ).then((myfile) => {
-      const imageRef = ref(storage, `images/${v4() + myfile.name}`);
-      uploadBytes(imageRef, myfile).then((snapshot) => {
-        console.log("deep then");
-        console.log(myfile);
-        alert("Image uploaded");
-        getDownloadURL(snapshot.ref).then((url) => {
-          setImageList((prev) => [...prev, url]);
-        });
-      });
-    }); ///luh umma beast
-
-    /////////////////////end of ma own jargon/////////////////
-  };
-
-  useEffect(() => {
-    listAll(imageListRef).then((response) => {
-      console.log(response);
-      response.items.forEach((item) => {
-        getDownloadURL(item).then((url) => {
-          setImageList((prev) => [...prev, url]);
-        });
-      });
-    });
-  }, []);
-
   return (
     <div className="App">
-      <input
-        type="file"
-        onChange={(event) => {
-          setImageUpload(event.target.files[0]);
-        }}
-      />
-      <button onClick={uploadImage}>Upload Image</button>
-      {imageList.map((url) => {
-        return <img src={url}></img>;
-      })}
-      {/* <header class="header el">
-        <h1>Header</h1>
+      <header class="header el">
+        <div className="custom-select">
+          <select>
+            <option value="">React Course</option>
+            <option value="">CSS Tutorials</option>
+          </select>
+          <span className="custom-arrow"></span>
+        </div>
+        <h1 class="logo">tutorial showcaser</h1>
+        <h1 class="about">About</h1>
       </header>
       <main class="container">
-        <div class="box">1</div>
-        <div class="box">2</div>
-        <div class="box">3</div>
-        <div class="box">4</div>
-        <div class="box">5</div>
-        <div class="box">6</div>
+        <div class="box">About</div>
+        <div class="box">
+          <iframe
+            src="https://www.nicholas-eruba.com"
+            title="W3Schools Free Online Web Tutorials"
+            width={"100%"}
+            height={"100%"}
+          ></iframe>
+        </div>
       </main>
       <aside class="sidebar el">
         <h2>Sidebar</h2>
       </aside>
       <footer class="footer el">
         <h2>Footer</h2>
-      </footer> */}
+      </footer>
     </div>
   );
 }
---
+++ src/utilities/oldcode.js
@@ -0,0 +1,98 @@
+////////////////////////zip stuff//////////
+// testa();
+//downloadFile(tutorials.css, "xy.zip");
+// getgit("El-Nico", "css_tutorials", "README.md").then((data) => {
+//   console.log("data shortly");
+//   console.log(data);
+// });
+// const navigate = useNavigate();
+
+// window.location.assign(
+//   "https://github.com/El-Nico/css_tutorials/archive/refs/heads/main.zip"
+// );
+
+// getgit2("El-Nico", "fewd_lab_3", "01_lesson").then((data) => {
+//     console.log(data);
+//   });
+
+//   //////////////////end of zip stuff//////////////
+//   const [imageUpload, setImageUpload] = useState(null);
+//   const [imageList, setImageList] = useState([]);
+
+//   const imageListRef = ref(storage, "images/");
+//   const uploadImage = () => {
+//     if (imageUpload == null) return;
+//     // const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+//     // uploadBytes(imageRef, imageUpload).then((snapshot) => {
+//     //   alert("Image uploaded");
+//     //   getDownloadURL(snapshot.ref).then((url) => {
+//     //     setImageList((prev) => [...prev, url]);
+//     //   });
+//     // });
+//     /////////////////////ma own jargon//////////////////////////////
+
+// const myFile = URLtoFile(
+//     "https://i.insider.com/602d763842b84000192f4072?width=1136&format=jpeg"
+//   ).then((myfile) => {
+//     const imageRef = ref(storage, `images/${v4() + myfile.name}`);
+//     uploadBytes(imageRef, myfile).then((snapshot) => {
+//       console.log("deep then");
+//       console.log(myfile);
+//       alert("Image uploaded");
+//       getDownloadURL(snapshot.ref).then((url) => {
+//         setImageList((prev) => [...prev, url]);
+//       });
+//     });
+//   }); ///luh umma beast
+
+//   /////////////////////end of ma own jargon/////////////////
+// };
+
+// useEffect(() => {
+//     listAll(imageListRef).then((response) => {
+//       console.log(response);
+//       response.items.forEach((item) => {
+//         getDownloadURL(item).then((url) => {
+//           setImageList((prev) => [...prev, url]);
+//         });
+//       });
+//     });
+//   }, []);
+
+{
+  /* <input
+        type="file"
+        onChange={(event) => {
+          setImageUpload(event.target.files[0]);
+        }}
+      />
+      <button onClick={uploadImage}>Upload Image</button>
+      {imageList.map((url) => {
+        return <img src={url}></img>;
+      })} */
+}
+
+// .box:first-child {
+//     background-color: blue;
+//     /* define how many columns and rows this item takes */
+//     /* grid-column-start: 1;
+//     grid-column-end: 4;
+//     grid-row-start: 1;
+//     grid-row-end: 3; */
+//     /* short form */
+//     /* grid-column: start/end */
+//     grid-column: 1 / 4;
+//     grid-row: 1/3;
+//     display: grid;
+//     /* align-content: center;
+//     justify-content: center; */
+//     /* place-content: alighn justify */
+//     place-content: end center;
+//     place-content: center;
+// }
+
+// .box:nth-child(2) {
+//     background-color: purple;
+//     grid-column: 1 / 5;
+//     grid-row: 3 / 4;
+// }
